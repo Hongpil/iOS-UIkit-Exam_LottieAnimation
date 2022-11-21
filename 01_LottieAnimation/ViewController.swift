213ad733f35535061c6b9bd54888d8f3669a3feb
kimhongpil <idpil@kimhongpils-MacBook-Pro.local>
2022-11-21 17:36:14 +0900

    first commit

---
+++ 01_LottieAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01_LottieAnimation
+//
+//  Created by kimhongpil on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# [출처] https://www.youtube.com/watch\?v\=E4AmzZ7R2Yc\&list\=PLgOlaPUIbynqRzpQBIdEDnjDdkVsjHqxK\&index\=3
